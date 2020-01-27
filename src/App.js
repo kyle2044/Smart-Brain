7f5c5d9498dfe875aa5cbb64ed42f3f1963f2a85
Kyle2044 <kyle.marquardt00@yahoo.com>
2020-01-27 16:42:31 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank'
+import './App.css';
+
+ const initialState = {
+  input: '',
+  imageUrl: '',
+  boxes: [],
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
+ 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+    }
+  
+loadUser = (data1) => {
+    this.setState({user: {
+      id: data1.id,
+      name: data1.name,
+      email: data1.email,
+      entries: data1.entries,
+      joined: data1.joined
+    }})
+  }
+
+  calculateFaceLocations = (data) => {
+    return data.outputs[0].data.regions.map(face => {
+      const clarifaiFace = face.region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    });
+  } 
+
+  displayBoundingBox = (boxes) => {
+    this.setState({boxes: boxes});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count }))
+        })
+        .catch(console.log)
+      }
+      this.displayBoundingBox(this.calculateFaceLocations(response))
+      })
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initialState)
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
+    return (
+      <div className="App">
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home' ?
+        <div>
+            <Logo />
+            <Rank 
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange} 
+              onButtonSubmit={this.onButtonSubmit} />
+              <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+          </div>
+          : (
+            route === 'signin' ?
+            <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+    return (
+    <div className='center ma'>
+        <div className='absolute mt2'>
+            <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
+            {boxes.map(box => {
+               return <div key={box.topRow} className='bounding-box' style={{left: box.leftCol, top: box.topRow, right: box.rightCol, bottom: box.bottomRow}}></div>
+            })
+            }
+        </div>
+    </div>
+    );
+}
+
+export default FaceRecognition;
