7bc4b2f61db38a3285eb0dc2e92c5e1b8f664368
Kyle2044 <kyle.marquardt00@yahoo.com>
2020-01-28 08:23:54 -0600

    changing directory

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ loadUser = (data1) => {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://cryptic-retreat-36232.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -75,7 +75,7 @@ loadUser = (data1) => {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://cryptic-retreat-36232.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://cryptic-retreat-36232.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
