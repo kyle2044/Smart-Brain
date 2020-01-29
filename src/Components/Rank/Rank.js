e52c9f166d4535b8c39fee86ebcd6d709f155428
Kyle2044 <kyle.marquardt00@yahoo.com>
2020-01-29 08:46:19 -0600

    change font color

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
     return (
       <div>
-        <div className='dark green f3'>
+        <div className='black f3'>
           {`${name}, your current entry count is...`}
         </div>
         <div className='dark green f1'>
